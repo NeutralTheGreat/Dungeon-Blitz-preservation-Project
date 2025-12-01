624b6f66634600da3a3314887c221eac2ee1b8bd
NeutralTheGreat <135655497+NeutralTheGreat@users.noreply.github.com>
2025-12-01 04:56:45 +0000

    Add group_members attribute to ClientSession
    Introduces a group_members list to the ClientSession class for tracking group membership. This prepares the session for future group-related features.

--- server/server.py
+++ server/server.py
@@ -83,6 +83,7 @@ class ClientSession:
         # Misc
         self.player_data = {}
         self.running = True
+        self.group_members = []
 
     def stop(self):
         self.running = False
